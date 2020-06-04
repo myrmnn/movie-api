199b0bddad2fd887191a4164df35efcc9dae5630
myrmnn <myrm.nn@gmail.com>
2020-06-04 14:22:02 -0400

    made code reusable with config object

--- app.js
+++ app.js
@@ -2,71 +2,37 @@ var key = 'b9718e3';
 var baseURL = 'http://www.omdbapi.com/';
 
 
-const fetchData = async (searchTerm) => {
-const response = await axios.get(baseURL, {
-    params: {
-        apikey: key,
-        s: searchTerm
-    }
 
-});
+createAutocomplete({
+    root: document.querySelector('.autocomplete'),
 
-if(response.data.Error){
-    return [];
-}
-
-return response.data.Search;
-};
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search For a Movie</b></label>
-<input class="input" />
-<div class="dropdown">  
-  <div class="dropdown-menu">
-    <div class="dropdown-content results"></div>
-  </div>
-</div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async e => {
-const movies = await fetchData(e.target.value);
-
-if(!movies.length){
-    dropdown.classList.remove('is-active')
-        return;
-}
-resultsWrapper.innerHTML = '';
- dropdown.classList.add('is-active');
-    for(let movie of movies){
-        const option = document.createElement('a');
+    renderOption(movie){
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
+        return `
         <img src="${imgSrc}" />
-        ${movie.Title}
-        `;
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active');
-            input.value = `${movie.Title}`;
-            onMovieSelect(movie);
-        })
+        ${movie.Title} (${movie.Year})
+        `
+    },
+    onOptionSelect(movie){
+        onMovieSelect(movie);
+    },
+    inputValue(movie){
+        return movie.Title
+    },
+    async fetchData(searchTerm){
+            const response = await axios.get(baseURL, {
+                params: {
+                    apikey: key,
+                    s: searchTerm
+                }
+            });
 
-        resultsWrapper.appendChild(option);
-    }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', e => {
-    if(!root.contains(e.target)){
-        dropdown.classList.remove('is-active');
-    }
-})
+            if(response.data.Error){
+                return [];
+            }
+            return response.data.Search;
+            }
+});
 
 const onMovieSelect = async movie => {
     const response = await axios.get(baseURL, {
@@ -97,6 +63,31 @@ const movieTemplate = movieDetail => {
         </div>
       </div>
 
+    </article>
+
+    <article class="notification is-$primary>
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+    </article>
+
+    <article class="notification is-primary>
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+    </article>
+
+    <article class="notification is-primary>
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+    </article>
+
+    <article class="notification is-primary>
+    <p class="title">${movieDetail.imdbR}ating</p>
+    <p class="subtitle">IMDB Rating</p>
+    </article>
+
+    <article class="notification is-primary>
+    <p class="title">${movieDetail.imdbVites}</p>
+    <p class="subtitle">IMDB Votes</p>
     </article>
     `;
   }
