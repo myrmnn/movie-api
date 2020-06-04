d1dd8a197b2a6834ec995ff3eb69b0aedb8e81e4
myrmnn <myrm.nn@gmail.com>
2020-06-03 20:56:36 -0400

    formatted js

--- app.js
+++ app.js
@@ -30,24 +30,20 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async e => {
 const movies = await fetchData(e.target.value);
-
 if(!movies.length){
     dropdown.classList.remove('is-active')
         return;
 }
-
 resultsWrapper.innerHTML = '';
  dropdown.classList.add('is-active');
     for(let movie of movies){
         const option = document.createElement('a');
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
-
         option.classList.add('dropdown-item');
         option.innerHTML = `
         <img src="${imgSrc}" />
         ${movie.Title}
         `;
-
         resultsWrapper.appendChild(option);
     }
 };
