d4abf5e0cc33dbd00a6891eaa878eeff7d0ca542
myrmnn <myrm.nn@gmail.com>
2020-06-04 09:46:01 -0400

    added second api request to display movie data on document

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+var key = 'b9718e3';
+var baseURL = 'http://www.omdbapi.com/';
+
+
 const fetchData = async (searchTerm) => {
-const response = await axios.get('http://www.omdbapi.com/', {
+const response = await axios.get(baseURL, {
     params: {
-        apikey: 'b9718e3',
+        apikey: key,
         s: searchTerm
     }
+
 });
 
 if(response.data.Error){
@@ -30,6 +35,7 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async e => {
 const movies = await fetchData(e.target.value);
+
 if(!movies.length){
     dropdown.classList.remove('is-active')
         return;
@@ -44,6 +50,12 @@ resultsWrapper.innerHTML = '';
         <img src="${imgSrc}" />
         ${movie.Title}
         `;
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = `${movie.Title}`;
+            onMovieSelect(movie);
+        })
+
         resultsWrapper.appendChild(option);
     }
 };
@@ -55,3 +67,37 @@ document.addEventListener('click', e => {
         dropdown.classList.remove('is-active');
     }
 })
+
+const onMovieSelect = async movie => {
+    const response = await axios.get(baseURL, {
+        params: {
+            apikey: key,
+            i: movie.imdbID
+        }
+    });
+
+    document.querySelector('.summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+    return `
+    <article class="media">
+
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" alt="movie poster">
+        </p>
+      </figure>
+  
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+
+    </article>
+    `;
+  }
+
